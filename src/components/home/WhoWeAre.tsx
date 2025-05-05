a4a998d5b701d13bb7b9c61171311aed868327ea
Ruby McNamee <rmcn333@aucklanduni.ac.nz>
2025-05-05 18:16:38 +1200

    feat: Added base for who we are page

--- src/components/home/WhoWeAre.tsx
+++ src/components/home/WhoWeAre.tsx
@@ -1,7 +1,18 @@
+import Image from 'next/image'
+import whoWeAre from '@/assets/tempteamwhoweare.jpg'
+
+
 export default function WhoWeAre() {
   return (
     <div>
-      <p>Who We Are</p>
+      <p className="flex justify-center text-lg">WHO WE ARE</p>
+      <Image
+        src={whoWeAre}
+        alt="Who We Are"
+        width={500}
+        height={500}
+        className="mx-auto my-4 rounded-lg"
+      />
     </div>
   )
 }
