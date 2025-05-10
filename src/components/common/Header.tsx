59327d1f97358f341e8003b9bae2a3a37bf7d359
Andrew Chen <ache337@aucklanduni.ac.nz>
2025-05-10 23:20:09 +1200

    header and hero visually done, no functionality and device support

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -6,24 +6,43 @@ export default function Header() {
   return (
     <div className="flex flex-col items-center"> 
       <div className="flex justify-center pt-4"> 
-        <div className="flex items-center gap-2">
+        <div className="flex items-center gap-4">
           <Image src={logo} alt="Hidden Treasure Logo"
-            width={30}
-            height={30}
+            width={40}
+            height={40}
           />
-          <h1>HIDDEN TREASURE</h1>
+          <p className="text-lg font-medium">HIDDEN TREASURE</p>
+          <button className="font-light absolute right-10 bg-[#13384E] text-white py-2 px-8 rounded transition-colors">
+          SIGN UP
+        </button>
         </div>
       </div>
 
       <nav>
         <div className="flex justify-center">
-          <div className="flex flex-wrap justify-center gap-6 md:gap-8 max-w-4xl w-full px-4 py-3">
-            <p>HOME</p>
-            <p>ABOUT US</p>
-            <p>BLOGS</p>
-            <p>EVENTS</p>
-            <p>OUR VALUES</p>
-            <p>CONTACT US</p>
+          <div className="flex flex-wrap justify-center gap-6 md:gap-8 px-4 py-3">
+            <a href="#">
+            <p className="hover:[text-shadow:0_0_0.5px_currentColor]">HOME</p></a>
+            <div className="inline-block h-[25px] w-0.5 bg-[#13384E]/40"></div>
+            
+            <a href="/aboutus">
+            <p className="hover:[text-shadow:0_0_0.5px_currentColor]">ABOUT US</p></a>
+            <div className="inline-block h-[25px] w-0.5 bg-[#13384E]/40"></div>
+
+            <a href="#blogs">
+            <p className="hover:[text-shadow:0_0_0.5px_currentColor]">BLOGS</p></a>
+            <div className="inline-block h-[25px] w-0.5 bg-[#13384E]/40"></div>
+
+            <a href="/events" className="hover:[text-shadow:0_0_0.5px_currentColor]">
+            <p className="hover:[text-shadow:0_0_0.5px_currentColor]">EVENTS</p></a>
+            <div className="inline-block h-[25px] w-0.5 bg-[#13384E]/40"></div>
+
+            <a href="/ourvalues">
+            <p className="hover:[text-shadow:0_0_0.5px_currentColor]">OUR VALUES</p></a>
+            <div className="inline-block h-[25px] w-0.5 bg-[#13384E]/40"></div>
+
+            <a href="/contactus">
+            <p className="hover:[text-shadow:0_0_0.5px_currentColor]">CONTACT US</p></a>
           </div>
         </div>
       </nav>
--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -1,7 +1,44 @@
+import elderly from '/public/elderly.jpg'
+import logo from '/public/biggerlogo.png'
+import Image from 'next/image'
+
 export default function HeroSection() {
   return (
-    <div>
-      <p>Hero Section</p>
+    <div className="flex flex-col items-centerpx-4 sm:px-8 md:px-12 lg:px-24 xl:px-36 2xl:px-48">
+      <div className="relative w-full aspect-video overflow-hidden rounded-b-3xl">
+        <Image 
+          src={elderly} 
+          alt="Elderly couple smiling" 
+          fill
+          className="object-cover opacity-60"
+          priority
+          sizes="100vw"
+        />
+        
+        <div className="absolute inset-0 flex items-center p-4 sm:p-8">
+          <div className="w-1/2 flex justify-center">
+            <Image 
+              src={logo} 
+              alt="Hidden Treasure Logo"
+              width={400}
+              height={400}
+            />
+          </div>
+
+          <div className="w-1/3 pl-4">
+            <h1 className="mt-15 text-8xl font-bold text-left">
+              HIDDEN<br />TREASURE
+            </h1>
+            <p className="mt-6 text-lg text-left max-w-md">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+              sed do eiusmod tempor incididunt ut labore et.
+            </p>
+            <button className="mt-8 bg-[#13384E] text-white font-bold px-8 py-2 rounded-lg transition-colors">
+              JOIN US
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
