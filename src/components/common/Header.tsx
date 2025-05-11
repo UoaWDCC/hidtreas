cdd4f641db64c1a96752646e6749c1f41dc65da6
Nathan Dalpatan <nathandalp@gmail.com>
2025-05-11 20:40:34 +1200

    html structure

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,7 +1,28 @@
 export default function Header() {
   return (
-    <div>
-      <p>Header</p>
-    </div>
+    <header className="">
+      <nav>
+        <ul>
+          <li>
+            <a href="">ABOUT US</a>
+          </li>
+          <li>
+            <a href="">BLOGS</a>
+          </li>
+          <li>
+            <a href="">EVENTS</a>
+          </li>
+        </ul>
+        <div></div>
+        <ul>
+          <li>
+            <a href="">CONTACT US</a>
+          </li>
+          <li>
+            <button>SIGN UP</button>
+          </li>
+        </ul>
+      </nav>
+    </header>
   )
 }
