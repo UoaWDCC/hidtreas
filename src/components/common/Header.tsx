3f41e47e125c9eb91c11abd600ce3e618ef61038
Nathan Dalpatan <nathandalp@gmail.com>
2025-05-12 15:23:48 +1200

    changed nav layout from flex to grid & minor changes

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -4,29 +4,31 @@ import LogoImage from '@/assets/logo.png'
 export default function Header() {
   return (
     <header className="bg-[#FDF4ED] text-[#13384E]">
-      <nav className="relative container mx-auto flex items-center justify-between py-6">
-        <ul className="flex space-x-8 items-center">
+      <nav className="relative container mx-auto grid grid-cols-[1fr_auto_1fr]">
+        <ul className="flex gap-8 items-center">
           <li>
-            <a href="" className="font-extrabold">
+            <a href="/about" className="font-extrabold">
               ABOUT US
             </a>
           </li>
           <li>
-            <a href="">BLOGS</a>
+            <a href="#">BLOGS</a>
           </li>
           <li>
-            <a href="">EVENTS</a>
+            <a href="#">EVENTS</a>
           </li>
         </ul>
-        <div className="absolute left-1/2 transform -translate-x-1/2 -top-3  bg-white rounded-full">
-          <Image src={LogoImage} alt="Logo" className="h-19 w-auto m-2.5" />
+        <div className="-translate-y-2  bg-white rounded-full">
+          <Image src={LogoImage} alt="Logo" className="h-19  w-auto p-2.5" />
         </div>
-        <ul className="flex space-x-8 items-center">
+        <ul className="flex gap-8 items-center place-content-end">
           <li>
-            <a href="">CONTACT US</a>
+            <a href="#">CONTACT US</a>
           </li>
           <li>
-            <button className="bg-[#13384E] text-white rounded px-4 py-2">SIGN UP</button>
+            <button className="bg-[#13384E] text-white rounded px-4 py-2 hover:cursor-pointer">
+              SIGN UP
+            </button>
           </li>
         </ul>
       </nav>
