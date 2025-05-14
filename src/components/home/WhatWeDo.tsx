35b730c695bdb78832c80095f40fcf92d10258b8
danielkimmm <kdaniel0315@gmail.com>
2025-05-14 23:03:30 +1200

    remove background and font in components and add images via imports

--- src/components/home/WhatWeDo.tsx
+++ src/components/home/WhatWeDo.tsx
@@ -1,15 +1,18 @@
+import Image from "next/image"
+import rockCarving from '@/assets/rockCarving.png'
+
 export default function WhatWeDo() {
     return (
-        <div className={"bg-[#D9D9D9]"}>
+        <div>
             <div className=" p-16 flex flex-col md:flex-row items-center justify-center max-w-6xl mx-auto gap-x-12">
                 <div className="md:w-1/2 text-center md:text-left">
-                    <h2 className="text-5xl font-bold text-[#13384E] mb-4">WHAT WE DO</h2>
-                    <p className="text-[#13384E] mb-4">
+                    <h2 className="text-5xl font-bold mb-4">WHAT WE DO</h2>
+                    <p className="mb-4">
                         Lorem ipsum dolor sit amet consectetur adipiscing elit. Consectetur
                         adipiscing elit quisque faucibLorem ipsum dolor sit amet consectetur
                         adipiscing elit. Consectetur adipiscing elit quisque faucibus ex sem.
                     </p>
-                    <p className="text-[#13384E] mb-4">
+                    <p className="mb-4">
                         Ex sapien vitae pellentesque seLorem ipsum dolor sit amet consectetur
                         adipiscing elit. Consectetur adipiscing elit quisque faucibus ex
                         sapien vitae. us ex sem.
@@ -22,8 +25,8 @@ export default function WhatWeDo() {
                 </div>
 
                 <div className="md:w-1/2 mt-8 md:mt-0">
-                    <img
-                        src="https://www.lovetaupo.com/media/2962417/jpeg_jm_chris_jolly_0014-large.jpg?anchor=center&mode=crop&width=900&height=1013&rnd=132551807629800000" // Replace with actual image path
+                    <Image
+                        src={rockCarving} // Replace with actual image path
                         alt="Rock Carving"
                         className="w-full rounded-md shadow-lg"
                     />
--- src/components/home/WhoWeAre.tsx
+++ src/components/home/WhoWeAre.tsx
@@ -1,21 +1,24 @@
+import Image from "next/image"
+import groupPic from '@/assets/groupPic.png'
+
 export default function WhoWeAre() {
     return (
-        <div className="bg-[#FDF4ED] py-16 px-6 flex justify-center">
+        <div className="py-16 px-6 flex justify-center">
             <div className="w-full max-w-4xl">
-                <h2 className="text-5xl font-bold text-[#13384E] text-center mb-8">
+                <h2 className="text-5xl font-bold text-center mb-8">
                     WHO WE ARE
                 </h2>
 
                 <div className="flex justify-center mb-8">
-                    <img
-                        src="https://nzlc.ac.nz/wp-content/uploads/2022/08/Untitled-design-1000x563.png" // Replace with actual path
+                    <Image
+                        src={groupPic} // Replace with actual path
                         alt="Group of people smiling"
-                        className="rounded-md shadow-lg w-full"
+                        className="rounded-md shadow-lg"
                     />
                 </div>
 
                 <div className="flex flex-col items-end text-right max-w-2xl ml-auto">
-                    <p className="text-[#13384E] mb-6">
+                    <p className="mb-6">
                         Lorem ipsum dolor sit amet consectetur adipiscing elit. Consectetur adipiscing elit
                         quisque faucibus ex sapien vitae. Ex sapien vitae pellentesque sem placerat in id.
                         Placerat in id cursus mi pretium tellus duis. Pretium tellus duis convallis tempus leo eu
