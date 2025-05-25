3cbcf1c9d220008234bdd2e383b5d1fde3f26776
Toshiro Mendoza <122713720+InZaneManInAZuite@users.noreply.github.com>
2025-05-26 02:27:09 +1200

    feat: created a custom 404 not found page

---
+++ src/app/(frontend)/[...notFound]/page.tsx
@@ -0,0 +1,20 @@
+import Footer from '@/components/common/Footer'
+import Header from '@/components/common/Header'
+import { IconError404 } from '@tabler/icons-react'
+
+export default async function Custom404Page() {
+  return (
+    <div className="h-screen flex flex-col">
+      <Header />
+      <div className="flex flex-col items-center justify-center h-full">
+        <IconError404 className="w-50 h-50" />
+        <h1 className="text-5xl font-bold mb-4">Page Not Found</h1>
+        <p className="text-lg mb-8">Sorry, the page you are looking for does not exist.</p>
+        <a href="/" className="text-blue-500 hover:underline">
+          Go back to Home
+        </a>
+      </div>
+      <Footer />
+    </div>
+  )
+}
