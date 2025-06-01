91c4f02b4bc3346541e0e5cf1ef85a300782c1b4
Ruby McNamee <rmcn333@aucklanduni.ac.nz>
2025-06-01 21:38:55 +1200

    feat:events-hero: coded most of hero

--- src/app/(frontend)/events/page.tsx
+++ src/app/(frontend)/events/page.tsx
@@ -1,10 +1,13 @@
 import Header from '@/components/common/Header'
 import Footer from '@/components/common/Footer'
+import Hero from '@/components/events/Hero'
 
 export default async function EventsPage() {
   return (
     <div>
       <Header />
+    
+      <Hero />
       
       <Footer />
     </div>
