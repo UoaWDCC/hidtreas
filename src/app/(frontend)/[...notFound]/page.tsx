394d10362eb0b26863dd34541c621333a10ad02c
Toshiro Mendoza <122713720+InZaneManInAZuite@users.noreply.github.com>
2025-06-06 21:32:01 +1200

    feat: added 404 in the background + some comments

--- src/app/(frontend)/[...notFound]/page.tsx
+++ src/app/(frontend)/[...notFound]/page.tsx
@@ -6,14 +6,19 @@ export default async function NotFound() {
   return (
     <div className="h-screen flex flex-col">
       <Header />
-      <div className="w-full h-full flex flex-col items-center justify-center text-center tracking-widest">
-        <div>
-          <h2 className="font-black text-[150px]">404</h2>
-        </div>
-        <div className="text-3xl font-black">
+
+      {/* Entire 404 Page */}
+      <div className="w-full h-full flex flex-col items-center justify-center text-center">
+        {/* Centerpiece with 404 message */}
+        <div className="text-3xl font-black relative flex flex-col items-center justify-center">
+          <div className="text-5xl font-[900] tracking-widest absolute z-10 opacity-30">
+            <h2 className="font-black text-[150px]">404</h2>
+          </div>
           <h1>Oh No!</h1>
           <h1>This Treasure Does Not Exist.</h1>
         </div>
+
+        {/* Links out of page */}
         <div className="underline font-semibold text-sm mt-5 flex flex-col gap-4 justify-center mt-20 uppercase tracking-wider">
           <a href="/">Return to Home</a>
           <a href="/events">Explore our Stories and Events</a>
