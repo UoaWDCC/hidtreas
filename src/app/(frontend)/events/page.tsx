3aa7347f8c27fef425847141598767313a3efc8c
Andrew Chen <ache337@aucklanduni.ac.nz>
2025-06-07 16:38:07 +1200

    feat: created event sign up modal

---
+++ src/app/(frontend)/events/page.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import Header from '@/components/common/Header'
+import Footer from '@/components/common/Footer'
+import EventsSignUpModal from '@/components/events/EventsSignUpModal'
+import { useState } from 'react'
+
+export default function EventsPage() {
+  const [signOpen, setSignOpen] = useState(false)
+
+  return (
+    <div className="home">
+      <Header />
+      <div className="flex justify-center mt-10 mb-10 ">
+        <button
+          className="bg-[#13384E] text-white hover:cursor-pointer w-[15rem] h-[5rem]"
+          onClick={() => setSignOpen(true)}
+        >
+          Temporary, Click Here
+        </button>
+      </div>
+      <Footer />
+      <EventsSignUpModal signOpen={signOpen} setSignOpen={setSignOpen} />
+    </div>
+  )
+}
