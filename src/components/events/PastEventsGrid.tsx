619bad32f62c6a2d69c679a6e0ef05680e1f4036
Ruby McNamee <rmcn333@aucklanduni.ac.nz>
2025-08-29 13:54:38 +1200

    Edited all pages without responsivity to be mobile responsive

--- src/components/events/PastEventsGrid.tsx
+++ src/components/events/PastEventsGrid.tsx
@@ -2,13 +2,15 @@ import EventsCard from './EventsCard'
 
 export default function PastEvents() {
   return (
-    <div className="grid grid-cols-3 gap-10 w-fit">
+    <div className="md:flex max-w-7xl mx-auto space-y-[2vh] md:space-x-[4vw] md:space-y-0 px-1 pt-4 pb-[2vw] justify-between items-center">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 w-fit">
       <EventsCard />
       <EventsCard />
       <EventsCard />
       <EventsCard />
       <EventsCard />
       <EventsCard />
+    </div>
     </div>
   )
 }
--- src/components/events/PastEventsSection.tsx
+++ src/components/events/PastEventsSection.tsx
@@ -5,15 +5,13 @@ import BlueKoru from '@/assets/blue_koru.png'
 
 export default function PastEventsSection() {
   return (
-    <div className="relative py-50">
+    <div className="w-full max-w-full relative py-50">
       <Image
         src={BlueKoru}
         alt="Blue Koru decoration"
-        width={256}
-        className="absolute -right-10 top-20 rotate-180
-          "
+        className="absolute right-0 top-[17vh] md:top-[10vh] lg:top-[7vh] rotate-180 w-[20vw] md:w-[18vw] lg:w-[17vw]"
       />
-      <h3 className="bg-[#E6E1DE] inline text-5xl px-20 py-5 border-none rounded-t-xl font-bold">
+      <h3 className="bg-[#E6E1DE] inline text-l sm:text-xl md:text-3xl lg:text-5xl px-20 py-5 border-none rounded-t-xl font-bold">
         PREVIOUS EVENTS
       </h3>
       <div className="bg-[#E6E1DE] flex flex-col items-center gap-y-10 py-10">
