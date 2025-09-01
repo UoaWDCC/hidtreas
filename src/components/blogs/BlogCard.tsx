ea0f928529ef36a0715675b8d2f4d93cbe53b4d7
bryy <bryannayeo04@gmail.com>
2025-09-01 23:03:56 +0900

    make Blogs page responsive

--- src/components/blogs/BlogCard.tsx
+++ src/components/blogs/BlogCard.tsx
@@ -1,5 +1,4 @@
 import Image, { StaticImageData } from 'next/image'
-
 import React, { FunctionComponent } from 'react'
 
 // TODO: Change `imageUrl` to `string` once all images come from CMS
@@ -12,24 +11,22 @@ interface Props {
 const BlogCard: FunctionComponent<Props> = ({ title, description, imageUrl }) => {
   return (
     <div
-      className={
-        'border-2 border-[#13384E] rounded-xl w-full h-full p-8 flex flex-col justify-between hover:shadow-[0_0_20px_rgba(0,0,0,0.3)]  transition-shadow'
-      }
+      className="
+        group border-2 border-[#13384E] rounded-xl w-full h-full p-8 flex flex-col justify-between
+        shadow-md hover:shadow-xl hover:bg-[#13384E]/10 transition-all duration-300
+      "
     >
-      {/*image's container*/}
+      {/* Image container */}
       <div className="w-full h-48 sm:h-56 md:h-64 relative rounded-xl overflow-hidden mb-4">
         <Image src={imageUrl} alt={title} className="object-cover" layout="fill" />
       </div>
 
-      <h1 className={'text-3xl font-black self-start mb-2 clamp-2'}>{title}</h1>
+      {/* Title & Blog Description */}
+      <h1 className="text-3xl font-black self-start mb-2 clamp-2">{title}</h1>
 
-      <p className={'text-sm text-gray-800 self-start mb-4 clamp-5 min-h-[2rem]'}>{description}</p>
+      <p className="text-sm text-gray-800 self-start mb-4 clamp-5 min-h-[2rem]">{description}</p>
 
-      <button
-        className={
-          'border-2 border-[#13384E] rounded-md px-4 py-1 font-semibold hover:bg-[#13384E] hover:text-[#FDF4ED] transition'
-        }
-      >
+      <button className="border-2 border-[#13384E] rounded-md px-4 py-1 font-semibold hover:bg-[#13384E] hover:text-[#FDF4ED] transition-colors duration-300">
         READ MORE
       </button>
     </div>
