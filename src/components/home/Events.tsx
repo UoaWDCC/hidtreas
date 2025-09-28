599c347c7f8b614a05951f18803bb70de1a28d51
Andrew Chen <ache337@aucklanduni.ac.nz>
2025-09-28 23:29:43 +1300

    feat: added reatcivity to components

--- src/components/home/Events.tsx
+++ src/components/home/Events.tsx
@@ -8,13 +8,13 @@ import AnimatedSection from '../common/AnimatedSection'
 
 export default function Events() {
   return (
-    <section className="px-4 sm:px-[3vw] py-4 sm:py-[1vw] lg:pb-30 lg:pt-10">
-      <div className="relative w-full sm:w-[95%] mx-auto h-full sm:h-[85vh] md:h-[40vw]">
+    <section className="px-4 sm:px-[3vw] py-4 sm:py-[1vw] lg:pt-20">
+      <div className="relative w-full max-w-[1200px] mx-auto h-auto min-h-[70vh] px-4">
         <div className="relative w-full h-[100vh] flex items-center justify-center">
           <Image
             src={backgroundImage}
             alt="Background Image"
-            className="max-w-none w-[150vh] h-auto"
+            className="w-[90vw] min-w-[500px] h-auto max-w-none"
             priority
             style={{
               animation: 'bob 3s ease-in-out infinite 0.3s',
@@ -23,17 +23,17 @@ export default function Events() {
         </div>
         {/* Content */}
         <div className="absolute inset-0 z-20 flex items-center justify-center md:items-start md:justify-start md:pl-[2vw] md:pt-[6vw] px-4 sm:px-[3vw]">
-          <div className="flex flex-col items-center md:flex-col md:items-start md:gap-[2vw] w-full max-w-xs sm:max-w-none md:max-w-none">
-            <div className="flex flex-row items-center gap-4 animate-slide-in-left">
+          <div className="flex flex-col items-center md:items-start md:gap-[2vw] w-full max-w-xs sm:max-w-none md:max-w-none">
+            <div className="flex flex-row items-center gap-4 animate-slide-in-left pb-10 leading-tight tracking-wide animate-bob">
               {/* Logo */}
               <Image
                 src={feather}
                 alt="Feather Icon"
-                className="w-[80px] h-auto scale-x-[-1] animate-bob"
+                className="w-[12vw] max-w-[80px] h-auto scale-x-[-1]"
               />
               {/* Text */}
               <h2
-                className="text-3xl sm:text-[6vw] md:text-[4vw] font-bold leading-tight tracking-wide animate-bob"
+                className="text-3xl sm:text-[6vw] md:text-[4vw] font-bold "
                 style={{
                   animation: 'bob 3s ease-in-out infinite 0.3s',
                 }}
--- src/components/home/EventsCarousel.tsx
+++ src/components/home/EventsCarousel.tsx
@@ -28,18 +28,18 @@ export default function ImageCarousel() {
         {events.map((event, idx) => (
           <div
             key={event.id}
-            className="group flex-shrink-0 w-[60vw] sm:w-[40vw] md:w-[25vw] h-[50vh] snap-center relative rounded-xl overflow-hidden shadow-lg"
+            className="group flex-shrink-0 w-[40vw] sm:w-[40vw] md:w-[30vw] h-[30vh] sm:h-[50vh] md:wh-[30vh] snap-center relative rounded-xl overflow-hidden shadow-lg"
           >
             {/* Image */}
             <Image src={event.image || ''} alt={event.name} fill className="object-cover" />
 
             {/* Info strip */}
-            <div className=" absolute bottom-0 left-0 w-full overflow-y-auto max-w-full h-[60%] bg-[#fdf4ed] text-[#13384e] flex flex-col p-5 overflow-x-auto justify-start opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-              <h1 className="font-bold text-3xl">{event.name}</h1>
-              <h1 className="text-lg pt-2 pb-2">{event.description}</h1>
+            <div className=" absolute bottom-0 left-0 w-full overflow-y-auto max-w-full h-[60%] bg-[#fdf4ed] text-[#13384e] flex flex-col p-3 sm:p-5 overflow-x-auto justify-start opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+              <h1 className="font-bold text-xl sm:text-2xl md:text-3xl">{event.name}</h1>
+              <h1 className="text-sm sm:text-lg pt-2 pb-2">{event.description}</h1>
               <button
                 onClick={() => setSignOpen(true)}
-                className="px-6 py-3 outline-solid font-semibold rounded-lg hover:bg-gray-100 transition"
+                className="px-4 py-2 sm:px-6 sm:py-3 outline-solid font-semibold rounded-lg hover:bg-gray-100 transition hover:cursor-pointer text-sm sm:text-base md:text-lg"
               >
                 Join Us
               </button>
@@ -48,12 +48,14 @@ export default function ImageCarousel() {
         ))}
 
         {/* Sign-Up Box as the last slide */}
-        <div className="flex-shrink-0 w-[60vw] gap-10 sm:w-[40vw] md:w-[25vw] h-[50vh] snap-center flex flex-col items-center justify-center rounded-xl bg-[#13384E] text-white shadow-lg text-center">
-          <h1 className="text-4xl font-bold">WANT MORE EVENTS LIKE THESE?</h1>
-          <p>Stay in touch and never miss out!</p>
+        <div className="flex-shrink-0 gap-5 sm:gap-9 w-[40vw] sm:w-[40vw] md:w-[30vw] h-[30vh] sm:h-[50vh] md:wh-[30vh] snap-center flex flex-col items-center justify-center rounded-xl bg-[#13384E] text-white shadow-lg text-center">
+          <h1 className="text-1xl sm:text-2xl md:text-3xl pt-2 sm:pt-5 md:pt-1 font-bold">
+            WANT MORE EVENTS LIKE THESE?
+          </h1>
+          <p className="text-sm sm:text-base md:text-lg">Stay in touch and never miss out!</p>
           <button
             onClick={() => setSignOpen(true)}
-            className="px-6 py-3 outline-solid w-[80%] text-[#fff] font-semibold rounded-lg hover:bg-[#1c5370c1] transition"
+            className="px-4 py-2 sm:px-6 sm:py-3 outline-solid w-[85%] sm:w-[80%] text-[#fff] font-semibold rounded-lg hover:bg-[#1c5370c1] transition hover:cursor-pointer text-sm sm:text-base md:text-lg"
           >
             Join Us
           </button>
