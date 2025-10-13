72d5dbd327a66eaaa52879f9d98bd1e311827a50
Toshiro Mendoza <122713720+InZaneManInAZuite@users.noreply.github.com>
2025-10-14 05:45:10 +1300

    feat: created a preview of the blogs page on /admin/blogPreview/[slug]

---
+++ src/app/(payload)/admin/blogPreview/[slug]/page.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Header from '@/components/common/Header'
+import Footer from '@/components/common/Footer'
+import { notFound } from 'next/navigation'
+import { getBlogBySlug } from '@/lib/payload/blogs'
+import Template1 from '@/components/blogs/templates/Template1'
+import Template2 from '@/components/blogs/templates/Template2'
+
+const templates = {
+  template1: Template1,
+  template2: Template2,
+} as const
+
+export default async function BlogPage({ params }: { params: Promise<{ slug: string }> }) {
+  const { slug } = await params
+  const blog = await getBlogBySlug(slug)
+  if (!blog) return notFound()
+
+  const Template = templates[blog.template]
+
+  return (
+    <>
+      <Header />
+      <Template blog={blog} />
+    </>
+  )
+}
